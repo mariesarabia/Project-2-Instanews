eba259b636ccd74182187fd033114fa362318fe5
Marie <marie@Lawrences-MacBook-Air.local>
2017-07-20 15:16:08 -0700

    added new tasks, created new folders

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,16 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     watch = require('gulp-watch'),
     browserSync = require('browser-sync').create(),
-    eslint = require('gulp-eslint');
+    eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename');
+    prettyError = require('gulp-prettyerror')
 
-// Gulp tasks below 
+// GULP TASKS BELOW
 
-// Gulp Scripts Task
+// Gulp Scripts Task with lint added as a dependency
 gulp.task('scripts', ['lint'], function () {
     gulp.src('./js/*.js')
         .pipe(uglify()) //call the uglify function on the files
@@ -17,9 +22,9 @@ gulp.task('scripts', ['lint'], function () {
 });
 
 // Gulp Say Hello Task
-gulp.task('say_hello', function () {
-    console.log('hello!');
-});
+// gulp.task('say_hello', function () {
+//     console.log('hello!');
+// });
 
 //Gulp Task: Browser-sync (reloading browser)
 gulp.task('browser-sync', function () {
@@ -29,20 +34,35 @@ gulp.task('browser-sync', function () {
         }
     }); //end of browser-sync init
 
-    gulp.watch('./build/js/*.js').on('change', browserSync.reload);
+    gulp.watch(['./build/css/*.css', './build/js/*.js', 'index.html']).on('change', browserSync.reload);
 });
 
 //Gulp Watch Task
 gulp.task('watch', function () {
+    gulp.watch('./sass/*.scss', ['sass']);
     gulp.watch('./js/*.js', ['scripts']);
 });
 
 //Gulp Lint Task
 gulp.task('lint', function () {
-    return gulp.src('./js/*.js')
+    return gulp.src('./js/*.js') //works on the human file not build
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
+});
+
+//Gulp Sass Task
+gulp.task('sass', function() {
+   gulp.src('./sass/style.scss')
+      .pipe(prettyError())
+      .pipe(sass())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./build/css'))
+      .pipe(cssnano())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'));
 });
 
 //Gulp Default Task
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready( function () {
 
-
+ 
 
 }); //end of doc.ready 
