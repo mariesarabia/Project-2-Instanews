b970177dd0a796d1c056f3495903afda2f3c8221
Marie <marie@Lawrences-MacBook-Air.local>
2017-07-21 16:37:11 -0700

    updated _content partial, scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
+// on click you select your topic 
+// header collapses 
+// empty (go before)
+// loader pops up 
+// json fetch happens 
+// appending data
+//tell loader to disappear 
+//.fail
+
+
+
+
 $(document).ready( function () {
 
  // Built by LucyBot. www.lucybot.com
  var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
         url += '?' + $.param({
         'api-key': "a40deeadfb074dcb9a3b5ac209821856",
-        'q': "Sacramento"
         });
+
         $.ajax({
         url: url,
         method: 'GET',
         })
+
         .done(function(data) {
         console.log(data.results[1].title);
 
