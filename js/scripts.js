f84b0bd63f6b37fb17dc99f837a2a8733abfba71
Marie <marie@Lawrences-MacBook-Air.local>
2017-07-21 14:36:10 -0700

    added font-face, created partials, edited html and styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 $(document).ready( function () {
 
- 
+ // Built by LucyBot. www.lucybot.com
+ var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+        url += '?' + $.param({
+        'api-key': "a40deeadfb074dcb9a3b5ac209821856",
+        'q': "Sacramento"
+        });
+        $.ajax({
+        url: url,
+        method: 'GET',
+        })
+        .done(function(data) {
+        console.log(data.results[1].title);
+
+        $.each(data.results, function(index, myArticle)[
+            console.log(myArticle.title);
+            $('body').append('<p>' + myArticle.title + '</p>');
+        });
 
 }); //end of doc.ready 
