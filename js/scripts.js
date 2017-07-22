c5d216ca839e9a2e4ff43d06cb8eb7895e07dadc
Marie <marie@Lawrences-Air.hitronhub.home>
2017-07-22 14:28:34 -0700

    worked on JS but need to tidy code

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,38 @@
-// on click you select your topic 
-// header collapses 
-// empty (go before)
-// loader pops up 
-// json fetch happens 
-// appending data
-//tell loader to disappear 
-//.fail
-
-
-
-
 $(document).ready( function () {
 
- // Built by LucyBot. www.lucybot.com
- var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-        url += '?' + $.param({
-        'api-key': "a40deeadfb074dcb9a3b5ac209821856",
+$('select-section').on('change', function (){
+    var selectedSection = this.value;
+
+    var url = "https://api.nytimes.com/svc/topstories/v2/" + selectedSection + ".json";
+            url += '?' + $.param({      
+            'api-key': "a40deeadfb074dcb9a3b5ac209821856",  
+            });
+
+    $('.grid-stories ul').html('');  
+        
+    $.ajax({
+    url: url,
+    method: 'GET',})
+
+    .done(function(data){
+
+    var results = data.results;
+        var newsStoryWithImages = results.filter(function(newsStory){
+                return newsStory.multimedia.length !== 0;
+    });
+
+    $.each(newsStoryWithImages, function(index, newsStory) {
+
+    if (index < 12 ){ 
+        var articleAbstract = newsStory.abstract;
+        var articleImage = newsStory.multimedia[4].url;
+
+        $('.grid-stories ul').append('<li><article style="background-image:url(' + articleImage + ')">'+ articleAbstract + '</article></li>');
+        }
+            });
         });
+    });
 
-        $.ajax({
-        url: url,
-        method: 'GET',
-        })
 
-        .done(function(data) {
-        console.log(data.results[1].title);
 
-        $.each(data.results, function(index, myArticle)[
-            console.log(myArticle.title);
-            $('body').append('<p>' + myArticle.title + '</p>');
-        });
-
-}); //end of doc.ready 
+}); //end of doc.ready
