b08b4da190d48acf920958b1ac1c659cb7bf4662
Marie <marie@Lawrences-Air.hitronhub.home>
2017-07-22 17:57:02 -0700

    added more styles to grid-stories - background image and flex properties (not on sections as mentioned previously)

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,37 @@ $(document).ready( function () {
     //change event fires when the user selects a value from the dropdown with a mouse click
     $('#select-section').on('change', function () {
         
+        
         var selectedSection = this.value; //this.value represents the selected section
 
+        //adding parameters to url:
         var url = "https://api.nytimes.com/svc/topstories/v2/" + selectedSection + ".json";
             url += '?' + $.param({      
             'api-key': "a40deeadfb074dcb9a3b5ac209821856",  
             });
 
-    $('.grid-stories ul').html('');  
+            //empty the grid-stories list each time a section is selected: 
+            $('.grid-stories ul').html('');  
         
-    $.ajax({
-    url: url,
-    method: 'GET',})
+        //retrieve data from the server; ajax is used to perform an asyncrhonous http request - GET   
+        $.ajax({
+        url: url,
+        method: 'GET',
+        })
 
-    .done(function(data){
+        //chaining the .done method to our Ajax request above after successful response:
+        .done(function(data){
 
-    var results = data.results;
-        var newsStoryWithImages = results.filter(function(newsStory){
+            var results = data.results;
+
+            //.filter method constructs a new jQuery object from matching subset of only news stories with images
+            var newsStoryWithImages = results.filter(function(newsStory){
                 return newsStory.multimedia.length !== 0;
-    });
+            });
 
-    $.each(newsStoryWithImages, function(index, newsStory) {
+            console.log(newsStoryWithImages);
+            
+            $.each(newsStoryWithImages, function(index, newsStory) {
 
     if (index < 12 ){ 
         var articleAbstract = newsStory.abstract;
