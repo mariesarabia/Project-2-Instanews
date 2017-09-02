2be7982217ebc0ae40a405880b275ee40e13b309
Marie <marie@Lawrences-Air.hitronhub.home>
2017-09-02 16:24:47 -0700

    Reorganized properties within scss files and applied more consistent indentation and use of variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,61 @@
-$(document).ready( function () {
+$(document).ready(function() {
 
     //change event fires when the user selects a value from the dropdown with a mouse click
-    $('#select-section').on('change', function () {
-        
-        var selectedSection = this.value; //this.value represents the selected section
+    $('#select-section').on('change', function() {
 
-        if( selectedSection != '') {
+        let selectedSection = this.value; //this.value represents the selected section
+
+        if (selectedSection != '') {
             $('.main-container').addClass('loader');
             $('header').addClass('header-transform');
         } else {
             $('.main-container').removeClass('loader');
             $('header').removeClass('header-transform');
         }
-        
+
 
         //adding parameters to url:
-        var url = "https://api.nytimes.com/svc/topstories/v2/" + selectedSection + ".json";
-            url += '?' + $.param({      
-            'api-key': "a40deeadfb074dcb9a3b5ac209821856",  
-            });
+        let url = "https://api.nytimes.com/svc/topstories/v2/" + selectedSection + ".json";
+        url += '?' + $.param({
+            'api-key': "a40deeadfb074dcb9a3b5ac209821856",
+        });
 
         //empty the grid-stories list each time a section is selected: 
-        $('.grid-stories ul').html('');  
-        
+        $('.grid-stories ul').html('');
+
         //retrieve data from the server; ajax is used to perform an asynchronous http request - GET   
         $.ajax({
-        url: url,
-        method: 'GET',
+            url: url,
+            method: 'GET',
         })
 
         //chaining the .done method to our Ajax request above after successful response:
-        .done(function(data){
+        .done(function(data) {
 
-            var results = data.results;
+            let results = data.results;
 
             //.filter method constructs a new jQuery object from matching subset of only news stories with images
-            var newsStoryWithImages = results.filter(function(newsStory){
+            let newsStoryWithImages = results.filter(function(newsStory) {
                 return newsStory.multimedia.length !== 0;
             }); //multimedia is an array so can use .length
 
             console.log(newsStoryWithImages);
-            
+
             $('.main-container').removeClass("loader");
 
             $.each(newsStoryWithImages, function(index, newsStory) {
 
 
-                if (index < 12 ){ 
-                    var articleAbstract = newsStory.abstract;
-                    var articleUrl = newsStory.short_url;
-                    var articleImage = newsStory.multimedia[4].url;
+                if (index < 12) {
+                    let articleAbstract = newsStory.abstract;
+                    let articleUrl = newsStory.short_url;
+                    let articleImage = newsStory.multimedia[4].url;
 
-                // console.log(articleUrl);
+                    // console.log(articleUrl);
 
                     //populate the grid by inserting needed content -- images, abstract, overlay -- using the append method:
                     $('.grid-stories ul').append('<li><a target="_blank" href="' + articleUrl + '"><article style="background-image:url(' + articleImage + ')">' + '<div class="overlay" >' + '<p>' + articleAbstract + '</p>' + '</div>' + '</article></a></li>');
-                    }
+                }
             });
         });
     });
